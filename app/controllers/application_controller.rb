ff8c4bf045fbd63e1f34039e2d9bb9674145c3f6
Pierce de Jong <pierce.dejong45@gamil.com>
2019-02-06 08:40:05 -0700

    remove old code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,6 @@
 class ApplicationController < ActionController::Base
 
   helper_method :current_user
-  # def current_user
-  #   if session[:user_id] || cookies.signed[:permanent_user_id]
-  #     @current_user ||= User.find_bu(uuid: cookies.signed[:permanent_user_id] || session[:user_id])
-  #   else
-  #     @current_user = nil
-  #   end
-  # end
 
   def current_user
       return unless cookies.signed[:permanent_user_id] || session[:user_id]
