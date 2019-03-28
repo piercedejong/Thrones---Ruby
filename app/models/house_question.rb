abfd54d93e7f8aff58f74634f00f2698d9dc881d
Pierce de Jong <pierce.dejong45@gmail.com>
2019-03-27 21:49:22 -0600

    updated questions

--- app/models/house_question.rb
+++ app/models/house_question.rb
@@ -6,8 +6,8 @@ class HouseQuestion < ApplicationRecord
   def self.create_questions
     HouseQuestion.create(
       [
-        {qid:1, text:"Will the White Walkers attack anyone? ONE Point"},
-        {qid:2, text:"Will we see Ghost? ONE Point"},
+        {qid:1, text:"Will the White Walkers attack castle black? ONE Point"},
+        {qid:2, text:"Will Ghost make an appearance? ONE Point"},
         {qid:3, text:"Will Jon Snow find out who his parents are? TWO Points"}
       ]
     )
