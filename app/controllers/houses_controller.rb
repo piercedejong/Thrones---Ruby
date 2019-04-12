f78f13d52300db9f0750c218122c430994817b5c
Pierce de Jong <pierce.dejong45@gamil.com>
2019-04-12 01:46:26 -0600

    test

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -76,7 +76,7 @@ class HousesController < ApplicationController
           characters.delete(r)
         end
       end
-      if c.name.eql? "The Night King"
+      if c.name.eql? "Rhaegal"
         puts("Alive: "+alive.to_s)
         puts("Dead: "+dead.to_s)
         puts("Wight: "+wight.to_s)
