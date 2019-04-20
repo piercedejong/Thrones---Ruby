c12520202a3b8e9c8d65a7c5b2e661c34b145495
Pierce de Jong <pierce.dejong45@gamil.com>
2019-04-19 20:08:08 -0600

    test user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,13 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-    @user = User.find_by_username(params[:username].downcase) || User.find_by_email(params[:username].downcase)
-    if @user && @user.authenticate(params[:password]) && @user.paid
+    @user = User.find_by(username:params[:username].downcase) || User.find_by(email:params[:username].downcase)
+    if @user and @user.authenticate(params[:password])
       cookies.permanent.signed[:permanent_user_id] = @user.uuid
       session[:user_id] = @user.uuid
       redirect_to root_url, notice: "Logged in!"
+    elsif @user and !@user.paid
+      redirect_to root_url, notice: "Error: Account was deactivated due no payment"
     else
       redirect_to root_url, notice: "Error: Username or Password is invalid"
     end
