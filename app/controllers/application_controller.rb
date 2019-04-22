00c0ec922a18e8f9d1e9fec70823826b1014075b
Pierce de Jong <pierce.dejong45@gamil.com>
2019-04-21 21:00:16 -0600

    episode 3 preview

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_episode
-    @episode =  2
+    @episode =  3
   end
 
   def video
@@ -34,7 +34,7 @@ class ApplicationController < ActionController::Base
       "https://www.youtube.com/embed/kuLUyJdRvSU",
     ]
     x = rand(0..5)
-    return "https://www.youtube.com/embed/R6YCfVe4eR0"
+    return "https://www.youtube.com/embed/TdkS4Xazz7Q"
   end
 
   def first_place
