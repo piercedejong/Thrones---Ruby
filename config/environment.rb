ed845780ecdb6f4755b27ccaaa74176a789ae884
Pierce de Jong <pierce.dejong45@gmail.com>
2019-11-18 22:49:46 -0700

    Update environment.rb
    removed username and apssword

--- config/environment.rb
+++ config/environment.rb
@@ -12,8 +12,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => "wwdeathpool@gmail.com",
-   :password             => 'whsfomgy',
+   :user_name            => ENV["GMAIL_USERNAME"],
+   :password             => ENV["GMAIL_PASSWORD"],
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
